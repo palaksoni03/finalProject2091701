3eda40501bdd6102c99c736d8aa91219d9c814dc
Palak Soni <palaksoni1895@gmail.com>
2021-10-12 04:11:12 -0400

    minor changes

--- app/src/main/java/com/example/finalproject_2091701/ui/dashboard/DashboardFragment.java
+++ app/src/main/java/com/example/finalproject_2091701/ui/dashboard/DashboardFragment.java
@@ -15,12 +15,12 @@ import androidx.lifecycle.ViewModelProvider;
 import com.example.finalproject_2091701.R;
 import com.example.finalproject_2091701.databinding.FragmentDashboardBinding;
 import com.example.finalproject_2091701.model.Plant;
+import com.squareup.picasso.Picasso;
 
 public class DashboardFragment extends Fragment {
 
     private DashboardViewModel dashboardViewModel;
-    private DashboardFragmentArgs binding;
-    private LayoutInflater FragmentDashboardBinding;
+    private FragmentDashboardBinding binding;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -28,9 +28,20 @@ public class DashboardFragment extends Fragment {
                 new ViewModelProvider(this).get(DashboardViewModel.class);
 
         binding = FragmentDashboardBinding.inflate(inflater, container, false);
-        @NonNull Plant root = binding.getPlantDetail();
+        View root = binding.getRoot();
 
         Plant plant = DashboardFragmentArgs.fromBundle(getArguments()).getPlantDetail();
+        Picasso.get().load(plant.getImage()).into(binding.pkdImgv);
+        binding.pkdTxtpname.setText(plant.getName());
+
+        binding.habitTxt.setText("Habit: "+plant.getHabit());
+        binding.lifespanTxt.setText("Lifespan: "+plant.getLifespan());
+        binding.exposureTxt.setText("Exposure: "+plant.getExposure());
+        binding.waterTxt.setText("Water: "+plant.getWater());
+        binding.featureTxt.setText("Feature: "+plant.getFeatures());
+        binding.specieTxt.setText("Species: "+(CharSequence) plant.getSpecies());
+
+
         return root;
     }
 
